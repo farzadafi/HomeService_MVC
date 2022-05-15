054f4a556da6becb4c1fc038bce25fe0109af46c
farzad <farzadafi40@gmail.com>
2022-05-15 19:36:48 +0430

    In The Name Of God!

---
+++ src/main/java/com/example/HomeService_MVC/HomeServiceMvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.HomeService_MVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeServiceMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeServiceMvcApplication.class, args);
+	}
+
+}
