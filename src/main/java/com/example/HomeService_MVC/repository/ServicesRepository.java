ac892df4dd9bfea16e82af01445ffdeb4879c3a5
farzad <farzadafi40@gmail.com>
2022-05-16 19:33:50 +0430

    add findByServices method

--- src/main/java/com/example/HomeService_MVC/repository/ServicesRepository.java
+++ src/main/java/com/example/HomeService_MVC/repository/ServicesRepository.java
@@ -4,4 +4,5 @@ import com.example.HomeService_MVC.model.Services;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ServicesRepository extends JpaRepository<Services,Integer> {
+    Services findByServices(String servicesName);
 }
