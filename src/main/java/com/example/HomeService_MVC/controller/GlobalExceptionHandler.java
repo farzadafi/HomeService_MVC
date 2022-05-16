d3bbf667fc4b07602a631edde6bc365d53c1f8e3
farzad <farzadafi40@gmail.com>
2022-05-16 10:58:23 +0430

    creat GlobalExceptionHandler class

---
+++ src/main/java/com/example/HomeService_MVC/controller/GlobalExceptionHandler.java
@@ -0,0 +1,12 @@
+package com.example.HomeService_MVC.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+}
