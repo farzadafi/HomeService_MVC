3750e227d7ac7e0d9d9d8ac8d903fbd53f9b2e79
farzad <farzadafi40@gmail.com>
2022-05-16 10:10:22 +0430

    create config class for add new configuration

---
+++ src/main/java/com/example/HomeService_MVC/core/Config.java
@@ -0,0 +1,15 @@
+package com.example.HomeService_MVC.core;
+
+import org.dozer.DozerBeanMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Config {
+
+    @Bean
+    public DozerBeanMapper dozerBeanMapper(){
+        return new DozerBeanMapper();
+    }
+
+}
