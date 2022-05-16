681dc475cb9e8f2f395aeccdbb7e6b075b45b86c
farzad <farzadafi40@gmail.com>
2022-05-16 22:54:08 +0430

    add getAllServices method

--- src/main/java/com/example/HomeService_MVC/controller/ServicesController.java
+++ src/main/java/com/example/HomeService_MVC/controller/ServicesController.java
@@ -1,27 +1,48 @@
 package com.example.HomeService_MVC.controller;
 
-import com.example.HomeService_MVC.dto.services.ServicesSave;
+import com.example.HomeService_MVC.controller.exception.ServicesNotFoundException;
+import com.example.HomeService_MVC.dto.services.ServicesDTO;
+import com.example.HomeService_MVC.model.Services;
 import com.example.HomeService_MVC.service.impel.ServicesServiceImpel;
+import org.dozer.DozerBeanMapper;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
-@RequestMapping("/service")
+@RequestMapping("/services")
 public class ServicesController {
 
     private final ServicesServiceImpel servicesServiceImpel;
+    private final DozerBeanMapper mapper;
 
-    public ServicesController(ServicesServiceImpel servicesServiceImpel) {
+    public ServicesController(ServicesServiceImpel servicesServiceImpel, DozerBeanMapper mapper) {
         this.servicesServiceImpel = servicesServiceImpel;
+        this.mapper = mapper;
     }
 
     @PostMapping("/save")
-    public ResponseEntity<ServicesSave> save(@Valid @RequestBody ServicesSave servicesSave){
+    public ResponseEntity<ServicesDTO> save(@Valid @RequestBody ServicesDTO servicesSave){
         servicesServiceImpel.save(servicesSave);
         return ResponseEntity.ok(servicesSave);
     }
+
+    @GetMapping("/getAllServices")
+    public ResponseEntity<List<ServicesDTO>> getAllServices(){
+        List<Services> servicesList = servicesServiceImpel.findAll();
+        if(servicesList == null || servicesList.size() == 0)
+            throw  new ServicesNotFoundException("not services defined until now");
+        List<ServicesDTO> dtoList = new ArrayList<>();
+        for (Services s:servicesList
+             ) {
+            dtoList.add(mapper.map(s,ServicesDTO.class));
+        }
+        return ResponseEntity.ok(dtoList);
+    }
+
+
+
 }
