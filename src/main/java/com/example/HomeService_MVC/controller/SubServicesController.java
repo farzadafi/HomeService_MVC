48690aa4680194a97373426a1ce1de7afa1b35eb
farzad <farzadafi40@gmail.com>
2022-05-16 22:54:37 +0430

    create subServicesController and add svaeSubServices method

---
+++ src/main/java/com/example/HomeService_MVC/controller/SubServicesController.java
@@ -0,0 +1,34 @@
+package com.example.HomeService_MVC.controller;
+
+import com.example.HomeService_MVC.controller.exception.ServicesNotFoundException;
+import com.example.HomeService_MVC.dto.services.SubServicesDTO;
+import com.example.HomeService_MVC.model.Services;
+import com.example.HomeService_MVC.service.impel.ServicesServiceImpel;
+import com.example.HomeService_MVC.service.impel.SubServicesServiceImpel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/subServices")
+public class SubServicesController {
+
+    private final SubServicesServiceImpel subServicesServiceImpel;
+    private final ServicesServiceImpel servicesServiceImpel;
+
+    public SubServicesController(SubServicesServiceImpel subServicesServiceImpel, ServicesServiceImpel servicesServiceImpel) {
+        this.subServicesServiceImpel = subServicesServiceImpel;
+        this.servicesServiceImpel = servicesServiceImpel;
+    }
+
+    @PostMapping("/save")
+    public ResponseEntity<SubServicesDTO> saveSubServices(@Valid @RequestBody SubServicesDTO subServicesDTO){
+        Services services = servicesServiceImpel.findById(subServicesDTO.getServicesId()).orElseThrow(() -> new ServicesNotFoundException("This services not found!"));
+        subServicesServiceImpel.save(subServicesDTO,services);
+        return ResponseEntity.ok(subServicesDTO);
+    }
+}
