0edb1e870e9b3e50ac669c2929b0d5c47d809613
farzad <farzadafi40@gmail.com>
2022-05-16 22:53:47 +0430

    add serviceExceptionHandler method

--- src/main/java/com/example/HomeService_MVC/controller/GlobalExceptionHandler.java
+++ src/main/java/com/example/HomeService_MVC/controller/GlobalExceptionHandler.java
@@ -1,12 +1,22 @@
 package com.example.HomeService_MVC.controller;
 
+import com.example.HomeService_MVC.controller.exception.ServicesNotFoundException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(ServicesNotFoundException.class)
+    public ResponseEntity<String> serviceExceptionHandler(ServicesNotFoundException e) {
+        LOGGER.warn(e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
 
 }
