2e86c96909f945d4b8e6f1bfb95b2f644a154ddb
farzad <farzadafi40@gmail.com>
2022-05-16 19:34:03 +0430

    add findByServicesName method

--- src/main/java/com/example/HomeService_MVC/service/impel/ServicesServiceImpel.java
+++ src/main/java/com/example/HomeService_MVC/service/impel/ServicesServiceImpel.java
@@ -1,8 +1,32 @@
 package com.example.HomeService_MVC.service.impel;
 
+import com.example.HomeService_MVC.dto.services.ServicesSave;
+import com.example.HomeService_MVC.model.Services;
+import com.example.HomeService_MVC.repository.ServicesRepository;
+import org.dozer.DozerBeanMapper;
 import org.springframework.stereotype.Service;
 import com.example.HomeService_MVC.service.interfaces.ServicesService;
 
 @Service
 public class ServicesServiceImpel implements ServicesService {
+
+    private final ServicesRepository servicesRepository;
+    private final DozerBeanMapper mapper;
+
+    public ServicesServiceImpel(ServicesRepository servicesRepository, DozerBeanMapper mapper) {
+        this.servicesRepository = servicesRepository;
+        this.mapper = mapper;
+    }
+
+    @Override
+    public void save(ServicesSave servicesSave) {
+        Services services = mapper.map(servicesSave,Services.class);
+        System.out.println(services.getServices());
+        servicesRepository.save(services);
+    }
+
+    @Override
+    public Services findByServicesName(String servicesName) {
+        return servicesRepository.findByServices(servicesName);
+    }
 }
--- src/main/java/com/example/HomeService_MVC/service/interfaces/ServicesService.java
+++ src/main/java/com/example/HomeService_MVC/service/interfaces/ServicesService.java
@@ -1,4 +1,9 @@
 package com.example.HomeService_MVC.service.interfaces;
 
+import com.example.HomeService_MVC.dto.services.ServicesSave;
+import com.example.HomeService_MVC.model.Services;
+
 public interface ServicesService {
+    void save(ServicesSave servicesSave);
+    Services findByServicesName(String servicesName);
 }
