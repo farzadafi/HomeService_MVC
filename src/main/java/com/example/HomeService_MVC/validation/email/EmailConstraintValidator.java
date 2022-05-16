a768f35adbf851e49b4f6edf16712cb1cc6de0a5
farzad <farzadafi40@gmail.com>
2022-05-16 10:56:45 +0430

    fix isValid method

--- src/main/java/com/example/HomeService_MVC/validation/email/EmailConstraintValidator.java
+++ src/main/java/com/example/HomeService_MVC/validation/email/EmailConstraintValidator.java
@@ -1,9 +1,18 @@
 package com.example.HomeService_MVC.validation.email;
 
+import com.example.HomeService_MVC.service.impel.UserServiceImpel;
+
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 public class EmailConstraintValidator implements ConstraintValidator<Email,String> {
+
+    private final UserServiceImpel userServiceImpel;
+
+    public EmailConstraintValidator(UserServiceImpel userServiceImpel) {
+        this.userServiceImpel = userServiceImpel;
+    }
+
     @Override
     public void initialize(Email constraintAnnotation) {
         ConstraintValidator.super.initialize(constraintAnnotation);
@@ -11,6 +20,8 @@ public class EmailConstraintValidator implements ConstraintValidator<Email,Strin
 
     @Override
     public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
+        if(userServiceImpel.existsByEmail(email))
+            return false;
         String EMAIL_REGEX = "^[\\w-_\\.+]*[\\w-_\\.]\\@([\\w]+\\.)+[\\w]+[\\w]$";
         if (email.length() < 10)
             return false;
