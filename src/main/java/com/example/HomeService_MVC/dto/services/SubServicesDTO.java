e23aedfb27e2fcb72657b96cfa30f85b68898469
farzad <farzadafi40@gmail.com>
2022-05-16 22:55:22 +0430

    create SubServicesDTO

---
+++ src/main/java/com/example/HomeService_MVC/dto/services/SubServicesDTO.java
@@ -0,0 +1,31 @@
+package com.example.HomeService_MVC.dto.services;
+
+import com.example.HomeService_MVC.validation.description.Description;
+import com.example.HomeService_MVC.validation.name.Name;
+import com.example.HomeService_MVC.validation.subServicesName.SubServices;
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.validation.constraints.Min;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class SubServicesDTO {
+    @Column(nullable = false)
+    private Integer servicesId;
+
+    @Name
+    @SubServices
+    private String subServicesName;
+
+    @Min(value=1, message="must be equal or greater than 1")
+    @Column(nullable = false)
+    private Long minimalPrice;
+
+    @Description
+    @Column(nullable = false)
+    private String description;
+}
