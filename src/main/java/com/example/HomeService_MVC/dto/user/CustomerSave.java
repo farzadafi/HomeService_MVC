27d847055f7354a19865141c9e34c3f4be18f061
farzad <farzadafi40@gmail.com>
2022-05-16 10:58:09 +0430

    create CustomerSave DtO

---
+++ src/main/java/com/example/HomeService_MVC/dto/user/CustomerSave.java
@@ -0,0 +1,31 @@
+package com.example.HomeService_MVC.dto.user;
+
+import lombok.*;
+import com.example.HomeService_MVC.validation.email.Email;
+import com.example.HomeService_MVC.validation.name.Name;
+import com.example.HomeService_MVC.validation.password.Password;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CustomerSave {
+    @Column(nullable = false)
+    @Name
+    private String firstName;
+
+    @Column(nullable = false)
+    @Name
+    private String lastName;
+
+    @Column(unique = true,nullable = false)
+    @Email
+    private String email;
+
+    @Column(nullable = false)
+    @Password
+    private String password;
+}
