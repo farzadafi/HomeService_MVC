c1b20a5f85bdda0401aae8cc4da31e9feab11a96
farzad <farzadafi40@gmail.com>
2022-05-17 21:46:58 +0430

    add viewOffer and selectOffer method

--- src/main/java/com/example/HomeService_MVC/controller/OfferController.java
+++ src/main/java/com/example/HomeService_MVC/controller/OfferController.java
@@ -1,12 +1,17 @@
 package com.example.HomeService_MVC.controller;
 
 
+import com.example.HomeService_MVC.controller.exception.OfferNotFoundException;
 import com.example.HomeService_MVC.dto.offer.OfferDTO;
+import com.example.HomeService_MVC.model.Offer;
 import com.example.HomeService_MVC.service.impel.OfferServiceImpel;
+import org.dozer.DozerBeanMapper;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/offer")
@@ -22,5 +27,31 @@ public class OfferController {
     public ResponseEntity<String> placeAnOffer(@Valid @RequestBody OfferDTO offerDTO, @PathVariable("orderId") Integer orderId){
         offerServiceImpel.placeAnOffer(1,offerDTO,orderId);
         return ResponseEntity.ok("OK");
+    }
+
+    @GetMapping("/viewOffer/{orderId}")
+    public ResponseEntity<List<OfferDTO>> viewOffer(@PathVariable("orderId") Integer orderId){
+        List<Offer> offerList = offerServiceImpel.findAllByOrdersId(orderId);
+        if(offerList == null || offerList.size() == 0)
+            throw new OfferNotFoundException("You dont have any offer until now!");
+        else{
+            List<OfferDTO> dtoList = offerToOfferDTO(offerList);
+            return ResponseEntity.ok(dtoList);
+        }
+    }
+
+    @GetMapping("/selectOffer/{orderId}/{offerId}")
+    public ResponseEntity<String> selectOffer(@PathVariable("orderId") Integer orderId,@PathVariable("offerId") Integer offerID){
+        offerServiceImpel.selectOffer(orderId,offerID);
+        return ResponseEntity.ok("OK");
+    }
+
+    public List<OfferDTO> offerToOfferDTO(List<Offer> offerList){
+        List<OfferDTO> dtoList = new ArrayList<>();
+        for (Offer o:offerList
+        ) {
+            dtoList.add(new OfferDTO(o.getId(),o.getProposedPrice(),o.getDurationWork(),o.getStartTime()));
+        }
+        return dtoList;
     }
 }
