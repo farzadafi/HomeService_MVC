0579441c304c964f11abef8f889d606a6ce94b69
farzad <farzadafi40@gmail.com>
2022-05-17 20:57:53 +0430

    add findAllByCustomerId and getById and ... methods

--- src/main/java/com/example/HomeService_MVC/service/impel/OrderServiceImpel.java
+++ src/main/java/com/example/HomeService_MVC/service/impel/OrderServiceImpel.java
@@ -6,12 +6,14 @@ import com.example.HomeService_MVC.dto.order.OrderDTO;
 import com.example.HomeService_MVC.model.Customer;
 import com.example.HomeService_MVC.model.Order;
 import com.example.HomeService_MVC.model.SubServices;
+import com.example.HomeService_MVC.model.enumoration.OrderStatus;
 import com.example.HomeService_MVC.repository.OrderRepository;
 import org.dozer.DozerBeanMapper;
 import org.springframework.stereotype.Service;
 import com.example.HomeService_MVC.service.interfaces.OrderService;
 
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class OrderServiceImpel implements OrderService {
@@ -30,7 +32,7 @@ public class OrderServiceImpel implements OrderService {
 
 
     @Override
-    public void PlaceAnOffer(Integer customerId,OrderDTO orderDTO, Integer subServicesId) {
+    public void PlaceAnOrder(Integer customerId,OrderDTO orderDTO, Integer subServicesId) {
         Customer customer = customerServiceImpel.getById(customerId);
         SubServices subServices = subServicesServiceImpel.findById(subServicesId).orElseThrow(() -> new SubServicesNotFoundException("This subServices not found!"));
         if(subServices.getMinimalPrice() > orderDTO.getProposedPrice())
@@ -38,11 +40,27 @@ public class OrderServiceImpel implements OrderService {
         Order order = mapper.map(orderDTO,Order.class);
         order.setCustomer(customer);
         order.setSubService(subServices);
+        order.setOrderStatus(OrderStatus.EXPERT_SUGGESTION);
         orderRepository.save(order);
     }
 
     @Override
     public List<Order> findAllByCustomerId(Integer customerId) {
         return orderRepository.findAllByCustomerId(customerId);
+    }
+
+    @Override
+    public List<Order> findAllStartOrder(Integer customerId) {
+        return orderRepository.findAllStartedOrderByCustomerIdAndOrderStatus(customerId, OrderStatus.EXPERT_SUGGESTION,OrderStatus.EXPERT_SELECTION);
+    }
+
+    @Override
+    public List<Order> findAllStartedOrderByCity(String city, Set<SubServices> subServices) {
+        return orderRepository.findAllStartedOrderByCity(city,subServices,OrderStatus.EXPERT_SUGGESTION,OrderStatus.EXPERT_SELECTION);
+    }
+
+    @Override
+    public Order getById(Integer id) {
+        return orderRepository.getById(id);
     }
 }
