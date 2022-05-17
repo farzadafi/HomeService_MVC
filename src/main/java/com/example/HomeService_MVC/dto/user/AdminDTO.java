2be3ef97b6d785c7db91915e169cb639ba4f9c79
farzad <farzadafi40@gmail.com>
2022-05-17 14:27:58 +0430

    create AdminDTO class

---
+++ src/main/java/com/example/HomeService_MVC/dto/user/AdminDTO.java
@@ -0,0 +1,28 @@
+package com.example.HomeService_MVC.dto.user;
+
+import com.example.HomeService_MVC.validation.email.Email;
+import com.example.HomeService_MVC.validation.password.Password;
+import lombok.*;
+import javax.persistence.Column;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AdminDTO {
+    @Column(unique = true,nullable = false)
+    @Email
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String confPassword;
+
+    @Password
+    public String[] getPassword(){
+        return new String[]{password,confPassword};
+    }
+}
