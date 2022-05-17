76ed49a18a6cf8572c81f892c4c334b7d2ac1d72
farzad <farzadafi40@gmail.com>
2022-05-17 22:22:53 +0430

    add findAllByExpertIdAndStatus method

--- src/main/java/com/example/HomeService_MVC/repository/OfferRepository.java
+++ src/main/java/com/example/HomeService_MVC/repository/OfferRepository.java
@@ -1,10 +1,20 @@
 package com.example.HomeService_MVC.repository;
 
 import com.example.HomeService_MVC.model.Offer;
+import com.example.HomeService_MVC.model.enumoration.OrderStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface OfferRepository extends JpaRepository<Offer,Integer> {
     List<Offer> findAllByOrdersId(Integer orderId);
+
+    @Query
+    ("FROM Offer AS o WHERE o.expert.id = :expertId AND " +
+        "o.orders.orderStatus = :ordersStatus AND " +
+        "o.isAccepted = true " )
+    List<Offer> findAllByExpertIdAndStatus(@Param("expertId") Integer expertId,
+                                           @Param("ordersStatus") OrderStatus orderStatus);
 }
