370f5442e4da6b871e50654868e57eb548ef63e1
farzad <farzadafi40@gmail.com>
2022-05-17 09:56:59 +0430

    add findExpertByEmail method

--- src/main/java/com/example/HomeService_MVC/repository/ExpertRepository.java
+++ src/main/java/com/example/HomeService_MVC/repository/ExpertRepository.java
@@ -4,7 +4,9 @@ import com.example.HomeService_MVC.model.Expert;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ExpertRepository extends JpaRepository<Expert,Integer> {
     List<Expert> findAllByAcceptedFalse();
+    Optional<Expert> findExpertByEmail(String expertEmail);
 }
