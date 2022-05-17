e6a25dd3eafdbc0571fe2a7e0cfa7a82ceff2b4e
farzad <farzadafi40@gmail.com>
2022-05-17 21:47:14 +0430

    add findAllByOrdersId method

--- src/main/java/com/example/HomeService_MVC/repository/OfferRepository.java
+++ src/main/java/com/example/HomeService_MVC/repository/OfferRepository.java
@@ -3,5 +3,8 @@ package com.example.HomeService_MVC.repository;
 import com.example.HomeService_MVC.model.Offer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OfferRepository extends JpaRepository<Offer,Integer> {
+    List<Offer> findAllByOrdersId(Integer orderId);
 }
