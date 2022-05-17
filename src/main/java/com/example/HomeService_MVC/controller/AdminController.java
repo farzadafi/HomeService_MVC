086a1ae4222b1cbc040fed82016f7212dbe756f2
farzad <farzadafi40@gmail.com>
2022-05-17 10:58:09 +0430

    add showExpertSubServices and removeExpertSubServices method

--- src/main/java/com/example/HomeService_MVC/controller/AdminController.java
+++ src/main/java/com/example/HomeService_MVC/controller/AdminController.java
@@ -1,9 +1,11 @@
 package com.example.HomeService_MVC.controller;
 
 import com.example.HomeService_MVC.controller.exception.ExpertNotFoundException;
+import com.example.HomeService_MVC.dto.services.SubServicesDTO;
 import com.example.HomeService_MVC.dto.user.ExpertSave;
 import com.example.HomeService_MVC.dto.user.ExpertViewDTO;
 import com.example.HomeService_MVC.model.Expert;
+import com.example.HomeService_MVC.model.SubServices;
 import com.example.HomeService_MVC.service.impel.ExpertServiceImpel;
 import org.dozer.DozerBeanMapper;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @RestController
 @RequestMapping("/admin")
@@ -46,6 +49,28 @@ public class AdminController {
     @PostMapping("/addExpertToSubServices/{subServicesId}")
     public ResponseEntity<String> addExpertToSubServices(@RequestBody ExpertSave expertSave, @PathVariable Integer subServicesId){
         expertServiceImpel.addExpertToSubService(expertSave.getEmail(),subServicesId);
+        return ResponseEntity.ok("OK");
+    }
+
+    @GetMapping("/showExpertSubServices/{expertEmail}")
+    public ResponseEntity<List<SubServicesDTO>> showExpertSubServices(@PathVariable("expertEmail") String expertEmail){
+        Expert expert = expertServiceImpel.findByEmail(expertEmail).orElseThrow(() -> new ExpertNotFoundException("This Expert is not found!"));
+        Set<SubServices> subServicesSet = expert.getSubServices();
+        if(subServicesSet == null || subServicesSet.size() == 0)
+            return ResponseEntity.ok(null);
+        else{
+            List<SubServicesDTO> dtoList = new ArrayList<>();
+            for (SubServices s:subServicesSet
+            ) {
+                dtoList.add(mapper.map(s,SubServicesDTO.class));
+            }
+            return ResponseEntity.ok(dtoList);
+        }
+    }
+
+    @GetMapping("/removeExpertSubServices/{expertEmail}/{servicesId}")
+    public ResponseEntity<String> removeExpertSubServices(@PathVariable("expertEmail") String expertEmail, @PathVariable("servicesId") Integer servicesId){
+        expertServiceImpel.removeExpertSubServices(expertEmail,servicesId);
         return ResponseEntity.ok("OK");
     }
 }
