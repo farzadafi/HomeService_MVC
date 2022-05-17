ed76f61de906f94ac7efc1533c8d454f23088315
farzad <farzadafi40@gmail.com>
2022-05-17 09:58:15 +0430

    add findAllByServicesId method

--- src/main/java/com/example/HomeService_MVC/repository/SubServicesRepository.java
+++ src/main/java/com/example/HomeService_MVC/repository/SubServicesRepository.java
@@ -3,6 +3,9 @@ package com.example.HomeService_MVC.repository;
 import com.example.HomeService_MVC.model.SubServices;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SubServicesRepository extends JpaRepository<SubServices,Integer> {
     SubServices findBySubServicesName(String subServicesName);
+    List<SubServices> findAllByServicesId(Integer servicesId);
 }
