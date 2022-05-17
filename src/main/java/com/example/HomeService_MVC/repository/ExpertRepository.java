872fc1c1197473f82791d42dee7b47756fb7f820
farzad <farzadafi40@gmail.com>
2022-05-17 08:25:32 +0430

    add findAllAcceptedFalse method

--- src/main/java/com/example/HomeService_MVC/repository/ExpertRepository.java
+++ src/main/java/com/example/HomeService_MVC/repository/ExpertRepository.java
@@ -3,5 +3,8 @@ package com.example.HomeService_MVC.repository;
 import com.example.HomeService_MVC.model.Expert;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ExpertRepository extends JpaRepository<Expert,Integer> {
+    List<Expert> findAllByAcceptedFalse();
 }
