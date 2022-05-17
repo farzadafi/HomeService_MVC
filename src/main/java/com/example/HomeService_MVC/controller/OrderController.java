a6efce77e2844bf6a4ee5a2efb0091a168dfe744
farzad <farzadafi40@gmail.com>
2022-05-17 21:01:48 +0430

    add viewStartedOrder and viewStartedOrderByCity methods

--- src/main/java/com/example/HomeService_MVC/controller/OrderController.java
+++ src/main/java/com/example/HomeService_MVC/controller/OrderController.java
@@ -2,7 +2,9 @@ package com.example.HomeService_MVC.controller;
 
 import com.example.HomeService_MVC.controller.exception.OrderNotFoundException;
 import com.example.HomeService_MVC.dto.order.OrderDTO;
+import com.example.HomeService_MVC.model.Expert;
 import com.example.HomeService_MVC.model.Order;
+import com.example.HomeService_MVC.service.impel.ExpertServiceImpel;
 import com.example.HomeService_MVC.service.impel.OrderServiceImpel;
 import org.dozer.DozerBeanMapper;
 import org.springframework.http.ResponseEntity;
@@ -18,15 +20,17 @@ public class OrderController {
 
     private final OrderServiceImpel orderServiceImpel;
     private final DozerBeanMapper mapper;
+    private final ExpertServiceImpel expertServiceImpel;
 
-    public OrderController(OrderServiceImpel orderServiceImpel, DozerBeanMapper mapper) {
+    public OrderController(OrderServiceImpel orderServiceImpel, DozerBeanMapper mapper, ExpertServiceImpel expertServiceImpel) {
         this.orderServiceImpel = orderServiceImpel;
         this.mapper = mapper;
+        this.expertServiceImpel = expertServiceImpel;
     }
 
     @PostMapping("/placeAnOffer/{subServicesId}")
     public ResponseEntity<String> placeAnOffer(@Valid @RequestBody OrderDTO orderDTO, @PathVariable("subServicesId") Integer subServicesId){
-        orderServiceImpel.PlaceAnOffer(1,orderDTO,subServicesId);
+        orderServiceImpel.PlaceAnOrder(1,orderDTO,subServicesId);
         return ResponseEntity.ok("OK");
     }
 
@@ -36,12 +40,40 @@ public class OrderController {
         if(orderList == null || orderList.size() == 0)
             throw new OrderNotFoundException("You dont have any order until now!");
         else{
-            List<OrderDTO> dtoList = new ArrayList<>();
-            for (Order o:orderList
-            ) {
-                dtoList.add(mapper.map(o,OrderDTO.class));
-            }
+            List<OrderDTO> dtoList = orderToOrderDTO(orderList);
             return ResponseEntity.ok(dtoList);
         }
+    }
+
+    @GetMapping("/viewStartedOrder")
+    public ResponseEntity<List<OrderDTO>> viewStartedOrder(){
+        List<Order> orderList = orderServiceImpel.findAllStartOrder(1);
+        if(orderList == null || orderList.size() == 0)
+            throw new OrderNotFoundException("You dont have any order in started status!");
+        else{
+            List<OrderDTO> dtoList = orderToOrderDTO(orderList);
+            return ResponseEntity.ok(dtoList);
+        }
+    }
+
+    @GetMapping("/viewStartedOrderByCity")
+    public ResponseEntity<List<OrderDTO>> viewStartedOrderByCity(){
+        Expert expert = expertServiceImpel.getById(1);
+        List<Order> orderList = orderServiceImpel.findAllStartedOrderByCity(expert.getCity(),expert.getSubServices());
+        if(orderList == null || orderList.size() == 0)
+            throw new OrderNotFoundException("You dont have any order for your subServices and city!");
+        else{
+            List<OrderDTO> dtoList = orderToOrderDTO(orderList);
+            return ResponseEntity.ok(dtoList);
+        }
+    }
+
+    public List<OrderDTO> orderToOrderDTO(List<Order> orderList){
+        List<OrderDTO> dtoList = new ArrayList<>();
+        for (Order o:orderList
+        ) {
+            dtoList.add(mapper.map(o,OrderDTO.class));
+        }
+        return dtoList;
     }
 }
