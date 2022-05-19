b570b663b81306d4648f5500b8290e0613d90092
farzad <farzadafi40@gmail.com>
2022-05-19 21:36:56 +0430

    fix viewOffer and viewAcceptedOffer method

--- src/main/java/com/example/HomeService_MVC/controller/OfferController.java
+++ src/main/java/com/example/HomeService_MVC/controller/OfferController.java
@@ -1,16 +1,13 @@
 package com.example.HomeService_MVC.controller;
 
 
-import com.example.HomeService_MVC.controller.exception.OfferNotFoundException;
 import com.example.HomeService_MVC.dto.offer.OfferDTO;
-import com.example.HomeService_MVC.model.Offer;
 import com.example.HomeService_MVC.model.enumoration.OrderStatus;
 import com.example.HomeService_MVC.service.impel.OfferServiceImpel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -31,13 +28,8 @@ public class OfferController {
 
     @GetMapping("/viewOffer/{orderId}")
     public ResponseEntity<List<OfferDTO>> viewOffer(@PathVariable("orderId") Integer orderId){
-        List<Offer> offerList = offerServiceImpel.findAllByOrdersId(orderId);
-        if(offerList == null || offerList.size() == 0)
-            throw new OfferNotFoundException("You dont have any offer until now!");
-        else{
-            List<OfferDTO> dtoList = offerToOfferDTO(offerList);
-            return ResponseEntity.ok(dtoList);
-        }
+        List<OfferDTO> offerDTOList = offerServiceImpel.findAllByOrdersId(orderId);
+        return ResponseEntity.ok(offerDTOList);
     }
 
     @GetMapping("/selectOffer/{orderId}/{offerId}")
@@ -48,22 +40,7 @@ public class OfferController {
 
     @GetMapping("/viewAcceptedOffer")
     public ResponseEntity<List<OfferDTO>> viewAcceptedOffer(){
-        List<Offer> offerList = offerServiceImpel.findAllByExpertIdAndStatus(1, OrderStatus.WAITING_FOR_EXPERT);
-        if(offerList == null || offerList.size() == 0)
-            throw new OfferNotFoundException("You dont have any Accepted offer until now!");
-        else{
-            List<OfferDTO> dtoList = offerToOfferDTO(offerList);
-            return ResponseEntity.ok(dtoList);
-        }
-    }
-
-
-    public List<OfferDTO> offerToOfferDTO(List<Offer> offerList){
-        List<OfferDTO> dtoList = new ArrayList<>();
-        for (Offer o:offerList
-        ) {
-            dtoList.add(new OfferDTO(o.getId(),o.getProposedPrice(),o.getDurationWork(),o.getStartTime()));
-        }
-        return dtoList;
+        List<OfferDTO> offerDTOList = offerServiceImpel.findAllByExpertIdAndStatus(1, OrderStatus.WAITING_FOR_EXPERT);
+        return ResponseEntity.ok(offerDTOList);
     }
 }
