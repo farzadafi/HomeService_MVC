8bbe3acda672bd85b5ddb3c1642632b6a1ce653a
farzad <farzadafi40@gmail.com>
2022-05-19 20:57:58 +0430

    fix Request mapping link

--- src/main/java/com/example/HomeService_MVC/controller/ExpertController.java
+++ src/main/java/com/example/HomeService_MVC/controller/ExpertController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 
 @RestController
-@RequestMapping("/Expert")
+@RequestMapping("/expert")
 public class ExpertController {
 
     private final ExpertServiceImpel expertServiceImpel;
