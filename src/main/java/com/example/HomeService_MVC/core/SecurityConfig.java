117881aa8a1376a68bbad7f6cb1c116ad8316ee8
farzad <farzadafi40@gmail.com>
2022-05-22 18:23:30 +0430

    add authntication config and bcryptPassword

--- src/main/java/com/example/HomeService_MVC/core/SecurityConfig.java
+++ src/main/java/com/example/HomeService_MVC/core/SecurityConfig.java
@@ -1,27 +1,46 @@
-/*
 package com.example.HomeService_MVC.core;
 
+import com.example.HomeService_MVC.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(jsr250Enabled = true,securedEnabled = true,prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final UserRepository userRepository;
+
+    public SecurityConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/api/**")
-                .authenticated()
                 .anyRequest()
                 .permitAll()
                 .and()
                 .formLogin();
     }
-}
 
- */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService( username -> userRepository.findByEmail(username)
+        .orElseThrow(() -> new UsernameNotFoundException("username " + username + " not found! ")))
+        .passwordEncoder(bCryptPasswordEncoder());
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
