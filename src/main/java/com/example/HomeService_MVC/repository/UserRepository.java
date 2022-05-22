67a4a7d86fa4b8759ec45e4dbe35cc085d9434c5
farzad <farzadafi40@gmail.com>
2022-05-22 18:26:24 +0430

    add findByEmail method

--- src/main/java/com/example/HomeService_MVC/repository/UserRepository.java
+++ src/main/java/com/example/HomeService_MVC/repository/UserRepository.java
@@ -4,6 +4,9 @@ import com.example.HomeService_MVC.model.base.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User,Integer>, JpaSpecificationExecutor<User> {
     boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
 }
