756cb5255fb4f3b11a798f9e23798fcef2b47179
farzad <farzadafi40@gmail.com>
2022-05-22 05:56:56 +0430

    fix save method and add convertExpertDTO method

--- src/main/java/com/example/HomeService_MVC/controller/ExpertController.java
+++ src/main/java/com/example/HomeService_MVC/controller/ExpertController.java
@@ -2,11 +2,16 @@ package com.example.HomeService_MVC.controller;
 
 import com.example.HomeService_MVC.dto.user.ExpertSave;
 import com.example.HomeService_MVC.dto.user.PasswordDTO;
+import com.example.HomeService_MVC.model.Expert;
+import com.example.HomeService_MVC.model.enumoration.Role;
 import com.example.HomeService_MVC.service.impel.ExpertServiceImpel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
+import java.io.IOException;
 
 @RestController
 @CrossOrigin
@@ -14,6 +19,7 @@ import javax.validation.Valid;
 public class ExpertController {
 
     private final ExpertServiceImpel expertServiceImpel;
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
 
     public ExpertController(ExpertServiceImpel expertServiceImpel) {
         this.expertServiceImpel = expertServiceImpel;
@@ -21,7 +27,7 @@ public class ExpertController {
 
     @PostMapping(value = "/save",consumes = { MediaType.MULTIPART_FORM_DATA_VALUE} )
     public String save(@Valid @ModelAttribute @RequestBody ExpertSave expertSave) {
-        expertServiceImpel.save(expertSave);
+        expertServiceImpel.save(convertExpertDTO(expertSave));
         return "OK";
     }
 
@@ -29,5 +35,19 @@ public class ExpertController {
     public ResponseEntity<String> updatePassword(@Valid @RequestBody PasswordDTO passwordDTO){
         expertServiceImpel.updatePassword(1,passwordDTO);
         return ResponseEntity.ok("OK");
+    }
+
+    private Expert convertExpertDTO(ExpertSave expertSave){
+        Expert expert = new Expert(expertSave.getFirstName(),expertSave.getLastName(),
+                expertSave.getEmail(),expertSave.getConfPassword(),
+                5000L, Role.EXPERT,expertSave.getCity()
+                ,null,0);
+        try {
+            expert.setImage(expertSave.getImage().getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+            LOGGER.warn(e.getMessage());
+        }
+        return expert;
     }
 }
