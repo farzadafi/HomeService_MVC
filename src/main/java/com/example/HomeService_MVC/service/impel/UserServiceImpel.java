1eec505e293b3c726994edf0eb34c3800bb52a43
farzad <farzadafi40@gmail.com>
2022-05-23 11:06:07 +0430

    add login method

--- src/main/java/com/example/HomeService_MVC/service/impel/UserServiceImpel.java
+++ src/main/java/com/example/HomeService_MVC/service/impel/UserServiceImpel.java
@@ -1,8 +1,11 @@
 package com.example.HomeService_MVC.service.impel;
 
+import com.example.HomeService_MVC.model.Expert;
 import com.example.HomeService_MVC.model.base.User;
+import com.example.HomeService_MVC.model.enumoration.Role;
 import com.example.HomeService_MVC.repository.UserRepository;
 import com.example.HomeService_MVC.service.interfaces.UserService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -19,6 +22,28 @@ public class UserServiceImpel implements UserService {
     @Override
     public Optional<User> findByEmail(String email) {
         return userRepository.findByEmail(email);
+    }
+
+    @Override
+    public String login(String email, String password) {
+        User user = userRepository.findByEmail(email).orElseThrow( () -> new UsernameNotFoundException(email + " not found!"));
+        if(!user.getPassword().equals(password))
+            return "ERROR";
+        else{
+            if(user.getRole().equals(Role.ROLE_ADMIN))
+                return "ADMIN";
+            else if(user.getRole().equals(Role.ROLE_CUSTOMER))
+                return "CUSTOMER";
+            else if(user.getRole().equals(Role.ROLE_EXPERT)){
+                Expert expert = (Expert) user;
+                if(expert.isAccepted())
+                    return "EXPERT";
+                else
+                    return "EXPERT_FALSE";
+            }
+            else
+                return "ERROR";
+        }
     }
 
 
