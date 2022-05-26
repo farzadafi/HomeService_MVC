bdeff8b7a05e8f87483b4b43b2312814b0b61ab1
farzad <farzadafi40@gmail.com>
2022-05-26 22:19:18 +0430

    fix updateAdmin method

--- src/main/java/com/example/HomeService_MVC/service/interfaces/AdminService.java
+++ src/main/java/com/example/HomeService_MVC/service/interfaces/AdminService.java
@@ -1,7 +1,7 @@
 package com.example.HomeService_MVC.service.interfaces;
 
-import com.example.HomeService_MVC.dto.user.AdminDTO;
+import com.example.HomeService_MVC.dto.user.PasswordDTO;
 
 public interface AdminService {
-    void updateAdmin(AdminDTO adminDTO);
+    void updateAdmin(PasswordDTO passwordDTO);
 }
