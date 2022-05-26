50ef1fd717f3278a931e364b9d0fc3fe08959cb1
farzad <farzadafi40@gmail.com>
2022-05-26 22:18:18 +0430

    fix updateAdmin method

--- src/main/java/com/example/HomeService_MVC/service/impel/AdminServiceImpel.java
+++ src/main/java/com/example/HomeService_MVC/service/impel/AdminServiceImpel.java
@@ -1,27 +1,26 @@
 package com.example.HomeService_MVC.service.impel;
 
-import com.example.HomeService_MVC.dto.user.AdminDTO;
+import com.example.HomeService_MVC.dto.user.PasswordDTO;
 import com.example.HomeService_MVC.model.Admin;
+import com.example.HomeService_MVC.model.base.User;
 import com.example.HomeService_MVC.repository.UserRepository;
 import com.example.HomeService_MVC.service.interfaces.AdminService;
-import org.dozer.DozerBeanMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminServiceImpel implements AdminService {
 
-    private final DozerBeanMapper mapper;
     private final UserRepository userRepository;
 
-    public AdminServiceImpel(DozerBeanMapper mapper, UserRepository userRepository) {
-        this.mapper = mapper;
+    public AdminServiceImpel( UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
 
     @Override
-    public void updateAdmin(AdminDTO adminDTO) {
-        Admin admin = mapper.map(adminDTO,Admin.class);
-        userRepository.save(admin);
+    public void updateAdmin(PasswordDTO passwordDTO) {
+        User user = userRepository.getById(1);
+        user.setPassword(passwordDTO.getSinglePassword());
+        userRepository.save(user);
     }
 }
