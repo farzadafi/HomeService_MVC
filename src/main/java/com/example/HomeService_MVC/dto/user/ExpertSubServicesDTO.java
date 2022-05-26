9bef73eeb3cf6e33092063546bc7b47da6399bbd
farzad <farzadafi40@gmail.com>
2022-05-26 08:51:26 +0430

    create ExpertSubServicesDTO class

---
+++ src/main/java/com/example/HomeService_MVC/dto/user/ExpertSubServicesDTO.java
@@ -0,0 +1,13 @@
+package com.example.HomeService_MVC.dto.user;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ExpertSubServicesDTO {
+    private Integer id;
+    private String email;
+}
