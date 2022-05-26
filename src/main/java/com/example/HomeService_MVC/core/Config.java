4fc11e88312b77a3daea63f64f0bf1e648d5df51
farzad <farzadafi40@gmail.com>
2022-05-26 08:50:58 +0430

    add new config for security

--- src/main/java/com/example/HomeService_MVC/core/Config.java
+++ src/main/java/com/example/HomeService_MVC/core/Config.java
@@ -7,6 +7,9 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class Config {
@@ -26,6 +29,18 @@ public class Config {
             }
         });
         return factory;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer()
+    {
+        return new WebMvcConfigurer() {
+            @NonNull
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:8080");
+            }
+        };
     }
 
 }
