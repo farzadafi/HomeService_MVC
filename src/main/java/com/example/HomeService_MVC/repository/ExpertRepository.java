e32fb8380801a7d66d298c7cfbe698e7df83b9f4
farzad <farzadafi40@gmail.com>
2022-05-26 21:50:29 +0430

    add findAll method

--- src/main/java/com/example/HomeService_MVC/repository/ExpertRepository.java
+++ src/main/java/com/example/HomeService_MVC/repository/ExpertRepository.java
@@ -1,12 +1,19 @@
 package com.example.HomeService_MVC.repository;
 
 import com.example.HomeService_MVC.model.Expert;
+import com.example.HomeService_MVC.model.base.User;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.lang.NonNull;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface ExpertRepository extends JpaRepository<Expert,Integer> {
+public interface ExpertRepository extends JpaRepository<Expert,Integer>, JpaSpecificationExecutor<Expert> {
     List<Expert> findAllByAcceptedFalse();
     Optional<Expert> findExpertByEmail(String expertEmail);
+    @NonNull
+    List<Expert> findAll(Specification<Expert> specification);
+
 }
