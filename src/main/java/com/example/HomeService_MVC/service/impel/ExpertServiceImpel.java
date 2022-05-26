2ece76a7120d5363014167d832040b2687d3bd13
farzad <farzadafi40@gmail.com>
2022-05-26 21:52:18 +0430

    add filterExpert method

--- src/main/java/com/example/HomeService_MVC/service/impel/ExpertServiceImpel.java
+++ src/main/java/com/example/HomeService_MVC/service/impel/ExpertServiceImpel.java
@@ -2,17 +2,27 @@ package com.example.HomeService_MVC.service.impel;
 
 import com.example.HomeService_MVC.controller.exception.ExpertNotFoundException;
 import com.example.HomeService_MVC.controller.exception.SubServicesNotFoundException;
+import com.example.HomeService_MVC.dto.user.DynamicSearch;
 import com.example.HomeService_MVC.dto.user.PasswordDTO;
 import com.example.HomeService_MVC.model.Expert;
 import com.example.HomeService_MVC.model.SubServices;
+import com.example.HomeService_MVC.model.base.User;
+import com.example.HomeService_MVC.model.enumoration.Role;
 import com.example.HomeService_MVC.repository.ExpertRepository;
+import com.example.HomeService_MVC.repository.UserRepository;
 import com.example.HomeService_MVC.service.interfaces.ExpertService;
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 public class ExpertServiceImpel implements ExpertService {
@@ -97,4 +107,41 @@ public class ExpertServiceImpel implements ExpertService {
         expertRepository.save(expert);
     }
 
+    public List<Expert> filterExpert(DynamicSearch dynamicSearch){
+        Expert expert = new Expert(dynamicSearch.getFirstName(),
+                dynamicSearch.getLastName(),
+                dynamicSearch.getEmail(),null,null,
+                Role.ROLE_EXPERT,null,null,dynamicSearch.getStars());
+
+        List<Expert> experts = expertRepository.findAll(userSpecification(expert));
+        if(dynamicSearch.getService() == null && dynamicSearch.getService().isEmpty())
+            return experts;
+        SubServices subServices = subServicesServiceImpel.findBySubServicesName(dynamicSearch.getService());
+        if(subServices == null)
+            return experts;
+        return subServices.getExperts().stream().filter(experts::contains).collect(Collectors.toList());
+    }
+
+    private Specification<Expert> userSpecification(Expert expert){
+        return (userRoot, query, criteriaBuilder)
+                -> {
+            CriteriaQuery<Expert> criteriaQuery = criteriaBuilder.createQuery(Expert.class);
+            criteriaQuery.select(userRoot);
+
+            List<Predicate> predicates = new ArrayList<>();
+            if(expert.getRole() != null )
+                predicates.add(criteriaBuilder.equal(userRoot.get("role"),expert.getRole()));
+            if(expert.getFirstName() != null && !expert.getFirstName().isEmpty())
+                predicates.add(criteriaBuilder.equal(userRoot.get("firstName"),expert.getFirstName()));
+            if(expert.getLastName() != null && !expert.getLastName().isEmpty())
+                predicates.add(criteriaBuilder.equal(userRoot.get("lastName"),expert.getLastName()));
+            if(expert.getStars()!= null)
+                predicates.add(criteriaBuilder.equal(userRoot.get("stars"),expert.getStars()));
+            if(expert.getEmail() != null && !expert.getEmail().isEmpty())
+                predicates.add(criteriaBuilder.equal(userRoot.get("email"),expert.getEmail()));
+
+            criteriaQuery.where(predicates.toArray(new Predicate[0]));
+            return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+        };
+    }
 }
