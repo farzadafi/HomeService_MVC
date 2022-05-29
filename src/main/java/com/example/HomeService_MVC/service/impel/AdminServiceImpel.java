9fe5c0a3b95d4b0710d616138704b192d20c8ce3
farzad <farzadafi40@gmail.com>
2022-05-29 22:21:45 +0430

    fix updateAdmin method and encode password

--- src/main/java/com/example/HomeService_MVC/service/impel/AdminServiceImpel.java
+++ src/main/java/com/example/HomeService_MVC/service/impel/AdminServiceImpel.java
@@ -1,26 +1,29 @@
 package com.example.HomeService_MVC.service.impel;
 
+import com.example.HomeService_MVC.core.SecurityUtil;
 import com.example.HomeService_MVC.dto.user.PasswordDTO;
-import com.example.HomeService_MVC.model.Admin;
 import com.example.HomeService_MVC.model.base.User;
 import com.example.HomeService_MVC.repository.UserRepository;
 import com.example.HomeService_MVC.service.interfaces.AdminService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AdminServiceImpel implements AdminService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public AdminServiceImpel( UserRepository userRepository) {
+    public AdminServiceImpel(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
 
     @Override
     public void updateAdmin(PasswordDTO passwordDTO) {
-        User user = userRepository.getById(1);
-        user.setPassword(passwordDTO.getSinglePassword());
+        User user = SecurityUtil.getCurrentUser();
+        user.setPassword(bCryptPasswordEncoder.encode(passwordDTO.getSinglePassword()));
         userRepository.save(user);
     }
 }
