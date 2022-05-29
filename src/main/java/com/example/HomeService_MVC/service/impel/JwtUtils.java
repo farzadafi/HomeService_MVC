e22bffb938b60c57ae37d9d5366be4c33c4deb4a
farzad <farzadafi40@gmail.com>
2022-05-29 07:27:52 +0430

    create JwtUtils class for create token

---
+++ src/main/java/com/example/HomeService_MVC/service/impel/JwtUtils.java
@@ -0,0 +1,22 @@
+package com.example.HomeService_MVC.service.impel;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUtils {
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .setSubject(email)
+                .setExpiration(new java.util.Date(System.currentTimeMillis() + 86400000))
+                .signWith(SignatureAlgorithm.HS256, "secretkey")
+                .compact();
+    }
+
+    public String getUsername(String token) {
+        return Jwts.parser().setSigningKey("secretkey").parseClaimsJws(token).getBody().getSubject();
+    }
+
+}
