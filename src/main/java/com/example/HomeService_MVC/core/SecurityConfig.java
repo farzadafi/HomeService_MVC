62f4ae67a7f4b8a3d9e48462a05f2d85e6f732d4
farzad <farzadafi40@gmail.com>
2022-05-29 07:26:44 +0430

    fix configure method

--- src/main/java/com/example/HomeService_MVC/core/SecurityConfig.java
+++ src/main/java/com/example/HomeService_MVC/core/SecurityConfig.java
@@ -3,19 +3,15 @@ package com.example.HomeService_MVC.core;
 import com.example.HomeService_MVC.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -23,75 +19,41 @@ import java.util.Arrays;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserRepository userRepository;
+    private final JwtFilter jwtFilter;
+    private final CustomSuccessHandler customSuccessHandler;
 
-    public SecurityConfig(UserRepository userRepository) {
+    public SecurityConfig(UserRepository userRepository, JwtFilter jwtFilter, CustomSuccessHandler customSuccessHandler) {
         this.userRepository = userRepository;
+        this.jwtFilter = jwtFilter;
+        this.customSuccessHandler = customSuccessHandler;
     }
-
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        /*
-        http
+        http.csrf().disable()
+                .cors().and()
                 .authorizeRequests()
-                .anyRequest()
-                .permitAll();
-                //.and()
-                //.formLogin()
-                //.successHandler(successHandler)
-                //.loginPage("/templates/login.html")
-                //.permitAll();
-
-         */
-        http.cors().and().authorizeRequests().anyRequest().permitAll();
+                .antMatchers(HttpMethod.GET,"/order/**").permitAll()
+                .antMatchers(HttpMethod.POST,"/expert/**").permitAll()
+                .antMatchers(HttpMethod.GET,"/services/**").permitAll()
+                .antMatchers("/customer/**", "/expert/**","/service/**").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin().successHandler(customSuccessHandler);
     }
-
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService( username -> userRepository.findByEmail(username)
-        .orElseThrow(() -> new UsernameNotFoundException("username " + username + " not found! ")))
-        .passwordEncoder(bCryptPasswordEncoder());
+        auth.userDetailsService((email) -> userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("This " + email +" notFound!")));
     }
 
-    /*
+    @Override
     @Bean
-    CorsConfigurationSource corsConfigurationSource()
-    {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:8080"));
-        configuration.setAllowedMethods(Arrays.asList("GET","POST"));
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
-
-     */
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder(){
         return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    public void configure(WebSecurity web) {
-        web.ignoring().antMatchers("/customer/save","/expert/save","/admin/findByEmail","/services/save","/subServices/save",
-                "/user/findByEmail","admin/confirmExpert","/admin/addExpertToSubServices");
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH",
-                "DELETE", "OPTIONS"));
-        configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type",
-                "x-auth-token"));
-        configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
-        UrlBasedCorsConfigurationSource source = new
-                UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
     }
 }
