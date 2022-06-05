5796cbf99a1ee991d1f28dd70a2d7682fad1dfa4
farzad <farzadafi40@gmail.com>
2022-06-05 06:37:10 +0430

    create CaptchaValidator and add some field

---
+++ src/main/java/com/example/HomeService_MVC/service/impel/CaptchaValidator.java
@@ -0,0 +1,38 @@
+package com.example.HomeService_MVC.service.impel;
+
+import com.example.HomeService_MVC.dto.cptcha.CaptchaResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class CaptchaValidator {
+
+    private final Logger logger = LoggerFactory.getLogger(CaptchaValidator.class);
+
+    private static final String GOOGLE_RECAPTCHA_ENDPOINT = "https://www.google.com/recaptcha/api/siteverify";
+
+    @Value("${google.recaptcha.secret}")
+    private String recaptchaSecret;
+
+    public boolean validateCaptcha(String captchaResponse){
+        RestTemplate restTemplate = new RestTemplate();
+
+        MultiValueMap<String, String> requestMap = new LinkedMultiValueMap<>();
+        requestMap.add("secret", recaptchaSecret);
+        requestMap.add("response", captchaResponse);
+
+        CaptchaResponse apiResponse = restTemplate.postForObject(GOOGLE_RECAPTCHA_ENDPOINT, requestMap, CaptchaResponse.class);
+        logger.info("Captcha api response {}", apiResponse);
+        if(apiResponse == null){
+            return false;
+        }
+
+        return Boolean.TRUE.equals(apiResponse.getSuccess());
+    }
+
+}
