25b768b2d81bf83989491cf431c286e4e0a000ba
farzad <farzadafi40@gmail.com>
2022-06-09 21:06:24 +0430

    add ConfirmationTokenRepository class

---
+++ src/main/java/com/example/HomeService_MVC/repository/ConfirmationTokenRepository.java
@@ -0,0 +1,10 @@
+package com.example.HomeService_MVC.repository;
+
+import com.example.HomeService_MVC.model.ConfirmationToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ConfirmationTokenRepository extends JpaRepository<ConfirmationToken,Integer> {
+    Optional<ConfirmationToken> findByConfirmToken(String confirmationToken);
+}
