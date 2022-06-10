7c09e48616a16d80489183265d51a63d408a160c
farzad <farzadafi40@gmail.com>
2022-06-10 07:00:18 +0430

    fix save method and add confirmUserAccount method

--- src/main/java/com/example/HomeService_MVC/controller/ExpertController.java
+++ src/main/java/com/example/HomeService_MVC/controller/ExpertController.java
@@ -3,15 +3,25 @@ package com.example.HomeService_MVC.controller;
 import com.example.HomeService_MVC.dto.user.DynamicSearchDTO;
 import com.example.HomeService_MVC.dto.user.ExpertDTO;
 import com.example.HomeService_MVC.dto.user.PasswordDTO;
+import com.example.HomeService_MVC.model.ConfirmationToken;
 import com.example.HomeService_MVC.model.Expert;
 import com.example.HomeService_MVC.model.enumoration.Role;
+import com.example.HomeService_MVC.model.enumoration.UserStatus;
+import com.example.HomeService_MVC.service.impel.ConfirmTokenServiceImpel;
 import com.example.HomeService_MVC.service.impel.ExpertServiceImpel;
+import com.example.HomeService_MVC.service.impel.UserServiceImpel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.validation.Valid;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -24,15 +34,58 @@ public class ExpertController {
 
     private final ExpertServiceImpel expertServiceImpel;
     private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+    private final ConfirmTokenServiceImpel confirmTokenServiceImpel;
+    private final JavaMailSender mailSender;
+    private final UserServiceImpel userServiceImpel;
 
-    public ExpertController(ExpertServiceImpel expertServiceImpel) {
+
+    public ExpertController(ExpertServiceImpel expertServiceImpel, ConfirmTokenServiceImpel confirmTokenServiceImpel, JavaMailSender mailSender, UserServiceImpel userServiceImpel) {
         this.expertServiceImpel = expertServiceImpel;
+        this.confirmTokenServiceImpel = confirmTokenServiceImpel;
+        this.mailSender = mailSender;
+        this.userServiceImpel = userServiceImpel;
     }
 
     @PostMapping(value = "/save",consumes = { MediaType.MULTIPART_FORM_DATA_VALUE} )
     public String save(@Valid @ModelAttribute @RequestBody ExpertDTO expertSave) {
         expertServiceImpel.save(convertExpertDTO(expertSave));
+        ConfirmationToken confirmationToken = new ConfirmationToken(convertExpertDTO(expertSave));
+        confirmTokenServiceImpel.save(confirmationToken);
+        String verifyCode = ("please click on link for confirm your email!"
+                +"http://localhost:8080/customer/confirmAccount/"+confirmationToken.getConfirmToken());
+        sendVerificationMessage(expertSave.getEmail(),verifyCode);
         return "OK";
+    }
+
+    public void sendVerificationMessage(
+            String mail, String text) {
+        MimeMessage msg = mailSender.createMimeMessage();
+
+        MimeMessageHelper message;
+        try {
+            message = new MimeMessageHelper(msg, true);
+            message.setTo(mail);
+            message.setSubject("تایید ایمیل");
+            message.setText(text, true);
+        } catch (MessagingException e) {
+            System.out.println(e.getMessage());
+        }
+        mailSender.send(msg);
+    }
+
+    @GetMapping("/confirmAccount/{token}")
+    public String confirmUserAccount(@PathVariable("token") String confirmationToken ) {
+        ConfirmationToken token = confirmTokenServiceImpel.findByConfirmToken(confirmationToken);
+        if(token == null )
+            return "شما قبلا ایمیل خود را تایید کرده اید!";
+        Expert expert = (Expert) userServiceImpel.findByEmail(token.getUser().getEmail()).orElseThrow(() -> new UsernameNotFoundException("متاسفانه شما پیدا نشدید!"));
+        if(expert.isEnabled())
+            return "شما قبلا ایمیل خود را تایید کرده اید!";
+        expert.setEnabled(true);
+        expert.setUserStatus(UserStatus.WAITING_FOR_ACCEPT);
+        expertServiceImpel.updateEnable(expert);
+        confirmTokenServiceImpel.deleteToken(token);
+        return "ایمیل شما با موفقیت تایید شد!";
     }
 
     @PutMapping("/updatePassword")
