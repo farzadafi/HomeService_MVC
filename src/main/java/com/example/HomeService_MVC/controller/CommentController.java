3b503573b399fc0bccbf932d74125fba0a51c444
farzad <farzadafi40@gmail.com>
2022-06-10 13:50:24 +0430

    fix save comment method

--- src/main/java/com/example/HomeService_MVC/controller/CommentController.java
+++ src/main/java/com/example/HomeService_MVC/controller/CommentController.java
@@ -1,10 +1,10 @@
 package com.example.HomeService_MVC.controller;
 
+import com.example.HomeService_MVC.core.SecurityUtil;
 import com.example.HomeService_MVC.dto.comment.CommentDto;
 import com.example.HomeService_MVC.model.Customer;
 import com.example.HomeService_MVC.service.impel.CommentServiceImpel;
-import com.example.HomeService_MVC.service.impel.CustomerServiceImpel;
-import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -15,17 +15,16 @@ import javax.validation.Valid;
 public class CommentController {
 
     private final CommentServiceImpel commentServiceImpel;
-    private final CustomerServiceImpel customerServiceImpel;
 
-    public CommentController(CommentServiceImpel commentServiceImpel, CustomerServiceImpel customerServiceImpel) {
+    public CommentController(CommentServiceImpel commentServiceImpel) {
         this.commentServiceImpel = commentServiceImpel;
-        this.customerServiceImpel = customerServiceImpel;
     }
 
-    @PostMapping("/save")
-    public ResponseEntity<String> save(@Valid @RequestBody CommentDto commentDto) {
-        Customer customer = customerServiceImpel.getById(2);
-        commentServiceImpel.placeAComment(customer,commentDto,commentDto.getId());
-        return ResponseEntity.ok("OK");
+    @PreAuthorize("hasRole('CUSTOMER')")
+    @PostMapping("/save/{orderId}")
+    public String save(@Valid @RequestBody CommentDto commentDto,@PathVariable("orderId") Integer orderId) {
+        Customer customer = (Customer) SecurityUtil.getCurrentUser();
+        commentServiceImpel.placeAComment(customer,commentDto,orderId);
+        return "OK";
     }
 }
