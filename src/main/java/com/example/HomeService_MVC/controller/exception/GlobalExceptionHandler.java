654b4748cc60b36eb8127afba73034bf86ccf263
farzad <farzadafi40@gmail.com>
2022-06-17 21:07:36 +0430

    remove one unused import

--- src/main/java/com/example/HomeService_MVC/controller/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/HomeService_MVC/controller/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.example.HomeService_MVC.controller.exception;
 
-import com.example.HomeService_MVC.controller.exception.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataIntegrityViolationException;
