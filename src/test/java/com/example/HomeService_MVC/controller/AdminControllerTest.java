e6e1ffeaa9746c327eac86ab8b5c531b07914505
farzad <farzadafi40@gmail.com>
2022-07-04 14:50:07 +0430

    create AdminControllerTest for junit test

--- src/test/java/com/example/HomeService_MVC/controller/AdminControllerTest.java
+++ src/test/java/com/example/HomeService_MVC/controller/AdminControllerTest.java
@@ -1,7 +1,27 @@
 package com.example.HomeService_MVC.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+@WithMockUser(username="admin",roles={"ADMIN","PRE_VERIFICATION_USER"})
 class AdminControllerTest {
 
 }
