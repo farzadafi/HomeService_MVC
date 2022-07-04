f1014b48967645a1d8ead91fddfe83c74b2bb9f8
farzad <farzadafi40@gmail.com>
2022-07-04 17:06:57 +0430

    add BConfirmExpert method

--- src/test/java/com/example/HomeService_MVC/controller/AdminControllerTest.java
+++ src/test/java/com/example/HomeService_MVC/controller/AdminControllerTest.java
@@ -4,8 +4,10 @@ import com.example.HomeService_MVC.model.Expert;
 import com.example.HomeService_MVC.model.enumoration.Role;
 import com.example.HomeService_MVC.model.enumoration.UserStatus;
 import com.example.HomeService_MVC.service.impel.ExpertServiceImpel;
+import org.junit.FixMethodOrder;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -21,6 +23,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @SpringBootTest
@@ -37,13 +40,26 @@ class AdminControllerTest {
                                    "kerman",null,0);
 
     @Test
-    public void getAllExpertFalse() throws Exception {
+    public void AGetAllExpertFalse() throws Exception {
         expertServiceImpel.save(expert);
         expert.setUserStatus(UserStatus.WAITING_FOR_ACCEPT);
         expertServiceImpel.updateEnable(expert);
+
         RequestBuilder requestBuilder = MockMvcRequestBuilders
                 .get("/admin/getAllExpertFalse")
                 .contentType(MediaType.APPLICATION_JSON);
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
+
+    @Test
+    public void BConfirmExpert() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/admin/confirmExpert/" + expert.getId())
+                .contentType(MediaType.APPLICATION_JSON);
+
         MvcResult result = mockMvc.perform(requestBuilder).andReturn();
         MockHttpServletResponse response = result.getResponse();
 
