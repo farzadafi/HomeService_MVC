0f1dd56f9c742f6b13fa7bfcf80c1eb86d50d4d2
farzad <farzadafi40@gmail.com>
2022-07-06 12:23:02 +0430

    add one delete command(because these query run for every method)

--- src/test/resources/com/example/HomeService_MVC/controller/AdminControllerData.sql
+++ src/test/resources/com/example/HomeService_MVC/controller/AdminControllerData.sql
@@ -1,2 +1,3 @@
+delete from users where id = 1;
 insert into users(dtype,first_name,last_name,email,is_enabled,password,user_status,role) VALUES
     ('Expert','farzad','afshar','f@gmail.com',false,'aA1!aaa','WAITING_FOR_ACCEPT','ROLE_EXPERT');
