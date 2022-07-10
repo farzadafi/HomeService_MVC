cb334bbb258475382f99005dad2bc696131965bc
farzad <farzadafi40@gmail.com>
2022-07-10 05:45:56 +0430

    add one setup method that run beforeAll and before class and insert data to database

--- src/test/java/com/example/HomeService_MVC/controller/AdminControllerTest.java
+++ src/test/java/com/example/HomeService_MVC/controller/AdminControllerTest.java
@@ -1,27 +1,32 @@
 package com.example.HomeService_MVC.controller;
 
 import com.example.HomeService_MVC.service.impel.ExpertServiceImpel;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.test.context.support.WithMockUser;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import javax.sql.DataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@Sql("AdminControllerData.sql")
 @RunWith(SpringRunner.class)
-@Sql(scripts = "adminControllerClean.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 @AutoConfigureMockMvc
 @SpringBootTest
 @WithMockUser(username="admin",roles={"ADMIN","PRE_VERIFICATION_USER"})
@@ -32,6 +37,15 @@ class AdminControllerTest {
 
     @Autowired
     private ExpertServiceImpel expertServiceImpel;
+
+    @BeforeAll
+    static void setup(@Autowired DataSource dataSource) {
+        try (Connection conn = dataSource.getConnection()) {
+            ScriptUtils.executeSqlScript(conn, new ClassPathResource("AdminControllerData.sql"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void GetAllExpertFalse() throws Exception {
@@ -47,12 +61,24 @@ class AdminControllerTest {
     @Test
     public void ConfirmExpert() throws Exception {
         RequestBuilder requestBuilder = MockMvcRequestBuilders
-                .get("/admin/confirmExpert/" + 2)
+                .get("/admin/confirmExpert/" + 1)
                 .contentType(MediaType.APPLICATION_JSON);
 
         MvcResult result = mockMvc.perform(requestBuilder).andReturn();
         MockHttpServletResponse response = result.getResponse();
 
         assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
+
+    @Test
+    public void addExpertToSubServices() throws Exception {
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .get("/admin/addExpertToSubServices/" + "f@gmail.com/" + 1 )
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+
+        assertEquals(HttpStatus.OK.value(),response.getStatus());
     }
 }
