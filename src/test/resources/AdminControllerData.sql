58dfc4ee57d304ed92abf68b8d9840618908f58b
farzad <farzadafi40@gmail.com>
2022-07-12 11:17:44 +0430

    add one query for add a new admin to DB

--- src/test/resources/AdminControllerData.sql
+++ src/test/resources/AdminControllerData.sql
@@ -2,3 +2,5 @@ insert into users(dtype,first_name,last_name,email,is_enabled,password,user_stat
     ('Expert','farzad','afshar','f@gmail.com',false,'aA1!aaa','WAITING_FOR_ACCEPT','ROLE_EXPERT');
 insert into services(services) VALUES ('Home');
 insert into sub_services(sub_services_name,services_id) VALUES ('clean',1);
+insert into users(dtype,first_name,last_name,email,is_enabled,password,user_status,role) VALUES
+    ('Admin','admin','admin','admin@gmail.com',false,'aA 1!aaa','ACCEPT','ROLE_ADMIN');
