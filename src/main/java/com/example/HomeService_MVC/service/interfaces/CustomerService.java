52bf195ab614216ab03973e32a0b96c9daccb0e2
farzad <farzadafi40@gmail.com>
2022-07-13 13:48:36 +0430

    remove getById method

--- src/main/java/com/example/HomeService_MVC/service/interfaces/CustomerService.java
+++ src/main/java/com/example/HomeService_MVC/service/interfaces/CustomerService.java
@@ -7,7 +7,6 @@ import com.example.HomeService_MVC.model.Customer;
 public interface CustomerService  {
     void save(Customer customer);
     void updatePassword(Customer customer, PasswordDto passwordDTO);
-    Customer getById(Integer id);
     void updateBalance(Customer customer);
     void updateEnable(Customer customer);
 }
