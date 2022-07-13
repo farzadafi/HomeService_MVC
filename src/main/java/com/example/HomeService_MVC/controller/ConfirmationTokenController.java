c1832c40d64c0974f470b5d28eeeb254a5b60906
farzad <farzadafi40@gmail.com>
2022-07-13 13:31:32 +0430

    add confirmUserAccount method

--- src/main/java/com/example/HomeService_MVC/controller/ConfirmationTokenController.java
+++ src/main/java/com/example/HomeService_MVC/controller/ConfirmationTokenController.java
@@ -1,4 +1,34 @@
 package com.example.HomeService_MVC.controller;
 
+import com.example.HomeService_MVC.model.ConfirmationToken;
+import com.example.HomeService_MVC.model.Customer;
+import com.example.HomeService_MVC.service.impel.ConfirmTokenServiceImpel;
+import com.example.HomeService_MVC.service.impel.CustomerServiceImpel;
+import com.example.HomeService_MVC.service.impel.UserServiceImpel;
+import lombok.AllArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/token")
+@AllArgsConstructor
 public class ConfirmationTokenController {
+
+    private final ConfirmTokenServiceImpel confirmTokenServiceImpel;
+    private final UserServiceImpel userServiceImpel;
+    private final CustomerServiceImpel customerServiceImpel;
+
+    public String confirmUserAccount(@PathVariable("token") String confirmationToken ) {
+        ConfirmationToken token = confirmTokenServiceImpel.findByConfirmToken(confirmationToken);
+        if(token == null )
+            return "شما قبلا ایمیل خود را تایید کرده اید!";
+        Customer customer = (Customer) userServiceImpel.findByEmail(token.getUser().getEmail()).orElseThrow(() -> new UsernameNotFoundException("متاسفانه شما پیدا نشدید!"));
+        if(customer.isEnabled())
+            return "شما قبلا ایمیل خود را تایید کرده اید!";
+        customer.setEnabled(true);
+        customerServiceImpel.updateEnable(customer);
+        confirmTokenServiceImpel.deleteToken(token);
+        return "ایمیل شما با موفقیت تایید شد!";
+    }
 }
