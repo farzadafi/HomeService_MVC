d9a79b7e8f57f23dbef133ca8e7b1740c50221b6
farzad <farzadafi40@gmail.com>
2022-07-14 14:26:49 +0430

    add findConfirmationTokensByUserId method

--- src/main/java/com/example/HomeService_MVC/service/interfaces/ConfirmTokenService.java
+++ src/main/java/com/example/HomeService_MVC/service/interfaces/ConfirmTokenService.java
@@ -3,11 +3,11 @@ package com.example.HomeService_MVC.service.interfaces;
 import com.example.HomeService_MVC.model.ConfirmationToken;
 import org.springframework.mail.SimpleMailMessage;
 
-import java.util.Optional;
 
 public interface ConfirmTokenService {
     void sendEmail(SimpleMailMessage email);
     void save(ConfirmationToken confirmationToken);
     ConfirmationToken findByConfirmToken(String confirmationToken);
     void deleteToken(ConfirmationToken confirmationToken);
+    ConfirmationToken findConfirmationTokensByUserId(Integer userId);
 }
