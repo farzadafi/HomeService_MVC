a73dce55b23b760456876decb625b36bc536b0f9
farzadafi <farzadafi40@gmail.com>
2022-07-15 16:29:39 +0430

    fix confirmUserAccount method

--- src/main/java/com/example/HomeService_MVC/controller/ConfirmationTokenController.java
+++ src/main/java/com/example/HomeService_MVC/controller/ConfirmationTokenController.java
@@ -2,8 +2,12 @@ package com.example.HomeService_MVC.controller;
 
 import com.example.HomeService_MVC.model.ConfirmationToken;
 import com.example.HomeService_MVC.model.Customer;
+import com.example.HomeService_MVC.model.Expert;
+import com.example.HomeService_MVC.model.base.User;
+import com.example.HomeService_MVC.model.enumoration.Role;
 import com.example.HomeService_MVC.service.impel.ConfirmTokenServiceImpel;
 import com.example.HomeService_MVC.service.impel.CustomerServiceImpel;
+import com.example.HomeService_MVC.service.impel.ExpertServiceImpel;
 import com.example.HomeService_MVC.service.impel.UserServiceImpel;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -18,18 +22,22 @@ public class ConfirmationTokenController {
     private final ConfirmTokenServiceImpel confirmTokenServiceImpel;
     private final UserServiceImpel userServiceImpel;
     private final CustomerServiceImpel customerServiceImpel;
+    private final ExpertServiceImpel expertServiceImpel;
 
     @GetMapping("/confirm/{token}")
     public String confirmUserAccount(@PathVariable("token") String confirmationToken ) {
         ConfirmationToken token = confirmTokenServiceImpel.findByConfirmToken(confirmationToken);
         if(token == null )
             return "شما قبلا ایمیل خود را تایید کرده اید!";
-        Customer customer = (Customer) userServiceImpel.findByEmail(token.getUser().getEmail())
+        User user = userServiceImpel.findByEmail(token.getUser().getEmail())
                 .orElseThrow(() -> new UsernameNotFoundException("متاسفانه ایمیل شما پیدا نشد"));
-        if(customer.isEnabled())
+        if(user.isEnabled())
             return "شما قبلا ایمیل خود را تایید کرده اید!";
-        customer.setEnabled(true);
-        customerServiceImpel.updateEnable(customer);
+        user.setEnabled(true);
+        if(user.getRole().equals(Role.ROLE_CUSTOMER))
+            customerServiceImpel.updateEnable((Customer) user);
+        else
+            expertServiceImpel.updateEnable((Expert) user);
         confirmTokenServiceImpel.deleteToken(token);
         return "ایمیل شما با موفقیت تایید شد!";
     }
