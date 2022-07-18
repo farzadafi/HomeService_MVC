7f1a2eacfca760bfee8525f9d9f9642dbe496e15
farzadafi <farzadafi40@gmail.com>
2022-07-18 15:05:15 +0430

    add updateExpertPassword method

--- src/test/java/com/example/HomeService_MVC/controller/ExpertControllerTest.java
+++ src/test/java/com/example/HomeService_MVC/controller/ExpertControllerTest.java
@@ -3,8 +3,11 @@ package com.example.HomeService_MVC.controller;
 
 import com.example.HomeService_MVC.dto.user.ExpertDto;
 import com.example.HomeService_MVC.model.ConfirmationToken;
+import com.example.HomeService_MVC.model.Expert;
+import com.example.HomeService_MVC.model.enumoration.Role;
 import com.example.HomeService_MVC.service.impel.ConfirmTokenServiceImpel;
 import lombok.extern.slf4j.Slf4j;
+import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
@@ -17,6 +20,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -48,6 +52,9 @@ class ExpertControllerTest {
     ExpertDto expertDto = new ExpertDto(null,"farzad","afshar"
             ,"farzadafi50@gmail.com","aA 1!aaa","aA 1!aaa","kerman",null);
 
+    Expert expert = new Expert("farzad","afshar","farzadafi50@gmail.com","aA 1!aaa"
+           , 50000L, Role.ROLE_EXPERT,"kerman",null,0);
+
     @Test
     @Order(1)
     public void save() throws Exception {
@@ -56,10 +63,10 @@ class ExpertControllerTest {
         try {
             content = Files.readAllBytes(path);
         } catch (final IOException e) {
-            log.warn("read image ");
+            log.error("read image from source");
         }
         MultipartFile multipartFile = new MockMultipartFile(path.getFileName().toString(),
-                                path.getFileName().toString(), "file/image", content);
+                                path.getFileName().toString(), "image/jpg", content);
         expertDto.setImage(multipartFile);
 
         RequestBuilder requestBuilder = MockMvcRequestBuilders
@@ -85,4 +92,23 @@ class ExpertControllerTest {
         if(!response.getContentAsString().equals("ایمیل شما با موفقیت تایید شد!"))
             fail();
     }
+
+    @Test
+    @Order(3)
+    public void updateExpertPassword() throws Exception {
+        expert.setId(1);
+        JSONObject passwordJson = new JSONObject();
+        passwordJson.put("password", "aA 1!aaa111");
+        passwordJson.put("confPassword", "aA 1!aaa111");
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/expert/updatePassword")
+                .with(SecurityMockMvcRequestPostProcessors.user(expert))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(passwordJson.toJSONString());
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
+
 }
