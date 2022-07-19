c4e234f2e3a372fb7068c7345ddec4abc1262140
farzadafi <farzadafi40@gmail.com>
2022-07-19 21:22:05 +0430

    add modelToDto method

--- src/main/java/com/example/HomeService_MVC/mapper/impel/CustomerMapperDecorator.java
+++ src/main/java/com/example/HomeService_MVC/mapper/impel/CustomerMapperDecorator.java
@@ -7,4 +7,29 @@ import com.example.HomeService_MVC.model.Customer;
 public class CustomerMapperDecorator implements CustomerMapper
 {
 
+    private final CustomerMapper customerMapper;
+
+    public CustomerMapperDecorator(CustomerMapper delegate) {
+        this.customerMapper = delegate;
+    }
+
+    @Override
+    public Customer dtoToModel(CustomerDto customerDto) {
+        Customer customer = customerMapper.dtoToModel(customerDto);
+        customer.setFirstName(customerDto.getFirstName());
+        customer.setLastName(customerDto.getLastName());
+        customer.setEmail(customerDto.getEmail());
+        customer.setPassword(customerDto.getPassword()[0]);
+        customer.setConfPassword(customerDto.getConfPassword());
+        return customer;
+    }
+
+    @Override
+    public CustomerDto modelToDto(Customer customer) {
+        CustomerDto customerDto = customerMapper.modelToDto(customer);
+        customerDto.setFirstName(customer.getFirstName());
+        customerDto.setLastName(customer.getLastName());
+        customerDto.setEmail(customer.getEmail());
+        return customerDto;
+    }
 }
