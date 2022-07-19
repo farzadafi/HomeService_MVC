0cc19a08af2befa2b43c46e0c742cbe5586d4cc7
farzadafi <farzadafi40@gmail.com>
2022-07-19 21:20:31 +0430

    create CustomerMapperDecorator and implement it from CustomerMapper

---
+++ src/main/java/com/example/HomeService_MVC/mapper/impel/CustomerMapperDecorator.java
@@ -0,0 +1,10 @@
+package com.example.HomeService_MVC.mapper.impel;
+
+import com.example.HomeService_MVC.dto.user.CustomerDto;
+import com.example.HomeService_MVC.mapper.interfaces.CustomerMapper;
+import com.example.HomeService_MVC.model.Customer;
+
+public class CustomerMapperDecorator implements CustomerMapper
+{
+
+}
