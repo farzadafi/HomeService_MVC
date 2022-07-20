2fad7e18433447631fdb7073f5b8124398b71096
farzadafi <farzadafi40@gmail.com>
2022-07-20 15:56:48 +0430

    create ExpertMapperDecorator class and implement it from ExpertMapper

---
+++ src/main/java/com/example/HomeService_MVC/mapper/impel/ExpertMapperDecorator.java
@@ -0,0 +1,6 @@
+package com.example.HomeService_MVC.mapper.impel;
+
+import com.example.HomeService_MVC.mapper.interfaces.ExpertMapper;
+
+public class ExpertMapperDecorator implements ExpertMapper {
+}
