b69cdeabea66c02aaf431a758893d46b19caf5e0
farzadafi <farzadafi40@gmail.com>
2022-07-20 16:16:27 +0430

    add dtoToModel method

--- src/main/java/com/example/HomeService_MVC/mapper/interfaces/ExpertMapper.java
+++ src/main/java/com/example/HomeService_MVC/mapper/interfaces/ExpertMapper.java
@@ -1,4 +1,17 @@
 package com.example.HomeService_MVC.mapper.interfaces;
 
+import com.example.HomeService_MVC.dto.user.ExpertDto;
+import com.example.HomeService_MVC.mapper.impel.ExpertMapperDecorator;
+import com.example.HomeService_MVC.model.Expert;
+import org.mapstruct.DecoratedWith;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+@DecoratedWith(ExpertMapperDecorator.class)
 public interface ExpertMapper {
+
+    ExpertMapper INSTANCE = Mappers.getMapper(ExpertMapper.class);
+
+    Expert dtoToModel(ExpertDto expertDto);
 }
