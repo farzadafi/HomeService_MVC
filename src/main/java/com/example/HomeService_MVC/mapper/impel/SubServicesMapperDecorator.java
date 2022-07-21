0555bc5abe893cb2614433efb40b45d670cf6c56
farzadafi <farzadafi40@gmail.com>
2022-07-21 06:46:55 +0430

    add dtoToModel method

--- src/main/java/com/example/HomeService_MVC/mapper/impel/SubServicesMapperDecorator.java
+++ src/main/java/com/example/HomeService_MVC/mapper/impel/SubServicesMapperDecorator.java
@@ -1,6 +1,24 @@
 package com.example.HomeService_MVC.mapper.impel;
 
+import com.example.HomeService_MVC.dto.services.SubServicesDto;
 import com.example.HomeService_MVC.mapper.interfaces.SubServicesMapper;
+import com.example.HomeService_MVC.model.SubServices;
 
 public class SubServicesMapperDecorator implements SubServicesMapper {
+
+    private final SubServicesMapper subServicesMapper;
+
+    public SubServicesMapperDecorator(SubServicesMapper subServicesMapper) {
+        this.subServicesMapper = subServicesMapper;
+    }
+
+    @Override
+    public SubServices dtoToModel(SubServicesDto subServicesDto) {
+        SubServices subServices = subServicesMapper.dtoToModel(subServicesDto);
+        subServices.setId(subServicesDto.getId());
+        subServices.setSubServicesName(subServicesDto.getSubServicesName());
+        subServices.setMinimalPrice(subServicesDto.getMinimalPrice());
+        subServices.setDescription(subServicesDto.getDescription());
+        return null;
+    }
 }
--- src/main/java/com/example/HomeService_MVC/mapper/interfaces/SubServicesMapper.java
+++ src/main/java/com/example/HomeService_MVC/mapper/interfaces/SubServicesMapper.java
@@ -1,6 +1,8 @@
 package com.example.HomeService_MVC.mapper.interfaces;
 
+import com.example.HomeService_MVC.dto.services.SubServicesDto;
 import com.example.HomeService_MVC.mapper.impel.SubServicesMapperDecorator;
+import com.example.HomeService_MVC.model.SubServices;
 import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
@@ -10,4 +12,6 @@ import org.mapstruct.factory.Mappers;
 public interface SubServicesMapper {
 
     SubServicesMapper INSTANCE = Mappers.getMapper(SubServicesMapper.class);
+
+    SubServices dtoToModel(SubServicesDto subServicesDto);
 }
