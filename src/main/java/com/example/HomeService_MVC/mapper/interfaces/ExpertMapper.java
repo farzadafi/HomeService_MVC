0928111d28984fcd05a89c3d513dfc40f55cc314
farzadafi <farzadafi40@gmail.com>
2022-07-21 06:10:13 +0430

    Revert "add modelToDto and modelListToDtoList method"
    This reverts commit 3fd8d44a1720b5bcf7e0f8c7374cdde7bcd55e99.

--- src/main/java/com/example/HomeService_MVC/mapper/interfaces/ExpertMapper.java
+++ src/main/java/com/example/HomeService_MVC/mapper/interfaces/ExpertMapper.java
@@ -3,12 +3,7 @@ package com.example.HomeService_MVC.mapper.interfaces;
 import com.example.HomeService_MVC.dto.user.ExpertDto;
 import com.example.HomeService_MVC.model.Expert;
 
-import java.util.List;
-
 public interface ExpertMapper {
 
     Expert dtoToModel(ExpertDto expertDto);
-    ExpertDto modelToDto(Expert expert);
-    List<ExpertDto> modelListToDtoList(List<Expert> expertList);
-
 }
