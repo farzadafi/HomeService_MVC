ec1b44c1441cc6c1d1762b768d714b02e94a53cf
farzadafi <farzadafi40@gmail.com>
2022-07-22 10:35:41 +0430

    fix placeAComment and pass comment instead of commentDto to this method and get current user form security in this method

--- src/main/java/com/example/HomeService_MVC/service/impel/CommentServiceImpel.java
+++ src/main/java/com/example/HomeService_MVC/service/impel/CommentServiceImpel.java
@@ -1,12 +1,11 @@
 package com.example.HomeService_MVC.service.impel;
 
-import com.example.HomeService_MVC.dto.comment.CommentDto;
+import com.example.HomeService_MVC.core.SecurityUtil;
 import com.example.HomeService_MVC.model.Comment;
 import com.example.HomeService_MVC.model.Customer;
 import com.example.HomeService_MVC.model.Expert;
 import com.example.HomeService_MVC.model.Offer;
 import com.example.HomeService_MVC.repository.CommentRepository;
-import org.dozer.DozerBeanMapper;
 import org.springframework.stereotype.Service;
 import com.example.HomeService_MVC.service.interfaces.CommentService;
 
@@ -14,21 +13,19 @@ import com.example.HomeService_MVC.service.interfaces.CommentService;
 public class CommentServiceImpel implements CommentService {
 
     private final OfferServiceImpel offerServiceImpel;
-    private final DozerBeanMapper mapper;
     private final CommentRepository commentRepository;
     private final ExpertServiceImpel expertServiceImpel;
 
-    public CommentServiceImpel(OfferServiceImpel offerServiceImpel, DozerBeanMapper mapper, CommentRepository commentRepository, ExpertServiceImpel expertServiceImpel) {
+    public CommentServiceImpel(OfferServiceImpel offerServiceImpel, CommentRepository commentRepository, ExpertServiceImpel expertServiceImpel) {
         this.offerServiceImpel = offerServiceImpel;
-        this.mapper = mapper;
         this.commentRepository = commentRepository;
         this.expertServiceImpel = expertServiceImpel;
     }
 
     @Override
-    public void placeAComment(Customer customer,CommentDto commentDto, Integer orderId) {
+    public void placeAComment(Comment comment, Integer orderId) {
+        Customer customer = (Customer) SecurityUtil.getCurrentUser();
         Offer offer = offerServiceImpel.findByOrderIdAndAcceptedTrue(orderId);
-        Comment comment = mapper.map(commentDto,Comment.class);
         Expert expert = offer.getExpert();
         comment.setCustomer(customer);
         comment.setExpert(expert);
