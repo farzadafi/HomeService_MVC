05507a6c4da3770578a97692e8b5ce9a1d37af66
farzadafi <farzadafi40@gmail.com>
2022-07-22 10:24:04 +0430

    create CommentMapperDecorator and implements form CommentMapper

---
+++ src/main/java/com/example/HomeService_MVC/mapper/impel/CommentMapperDecorator.java
@@ -0,0 +1,6 @@
+package com.example.HomeService_MVC.mapper.impel;
+
+import com.example.HomeService_MVC.mapper.interfaces.CommentMapper;
+
+public class CommentMapperDecorator implements CommentMapper {
+}
