dfce0879eff3f29448b48846d45422051dae7b1a
farzadafi <farzadafi40@gmail.com>
2022-07-22 10:36:04 +0430

    add dtoToModel method

--- src/main/java/com/example/HomeService_MVC/mapper/impel/CommentMapperDecorator.java
+++ src/main/java/com/example/HomeService_MVC/mapper/impel/CommentMapperDecorator.java
@@ -1,6 +1,27 @@
 package com.example.HomeService_MVC.mapper.impel;
 
+import com.example.HomeService_MVC.dto.comment.CommentDto;
 import com.example.HomeService_MVC.mapper.interfaces.CommentMapper;
+import com.example.HomeService_MVC.model.Comment;
+import org.mapstruct.DecoratedWith;
+import org.mapstruct.Mapper;
 
+@Mapper
+@DecoratedWith(CommentMapperDecorator.class)
 public class CommentMapperDecorator implements CommentMapper {
+
+    private final CommentMapper commentMapper;
+
+    public CommentMapperDecorator(CommentMapper commentMapper) {
+        this.commentMapper = commentMapper;
+    }
+
+    @Override
+    public Comment dtoToModel(CommentDto commentDto) {
+        Comment comment = commentMapper.dtoToModel(commentDto);
+        comment.setId(commentDto.getId());
+        comment.setComment(commentDto.getComment());
+        comment.setStars(commentDto.getStars());
+        return comment;
+    }
 }
--- src/main/java/com/example/HomeService_MVC/mapper/interfaces/CommentMapper.java
+++ src/main/java/com/example/HomeService_MVC/mapper/interfaces/CommentMapper.java
@@ -1,4 +1,12 @@
 package com.example.HomeService_MVC.mapper.interfaces;
 
+import com.example.HomeService_MVC.dto.comment.CommentDto;
+import com.example.HomeService_MVC.model.Comment;
+import org.mapstruct.factory.Mappers;
+
 public interface CommentMapper {
+
+    CommentMapper INSTANCE = Mappers.getMapper(CommentMapper.class);
+
+    Comment dtoToModel(CommentDto commentDto);
 }
