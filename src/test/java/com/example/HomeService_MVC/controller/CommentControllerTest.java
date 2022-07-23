4ee0095e2fc93ecbd0115225036e39cecd123649
farzadafi <farzadafi40@gmail.com>
2022-07-23 15:38:32 +0430

    add setup method for insert data to DB

--- src/test/java/com/example/HomeService_MVC/controller/CommentControllerTest.java
+++ src/test/java/com/example/HomeService_MVC/controller/CommentControllerTest.java
@@ -1,7 +1,34 @@
 package com.example.HomeService_MVC.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class CommentControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeAll
+    static void setup(@Autowired DataSource dataSource) {
+        try (Connection connection = dataSource.getConnection()) {
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource("CommentControllerData.sql"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
