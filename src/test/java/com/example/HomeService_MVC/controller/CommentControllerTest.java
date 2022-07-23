bc4b08d756bd2371eed3a81a8c7bcea37ab7fe2e
farzadafi <farzadafi40@gmail.com>
2022-07-23 16:05:17 +0430

    add placeAComment method test

--- src/test/java/com/example/HomeService_MVC/controller/CommentControllerTest.java
+++ src/test/java/com/example/HomeService_MVC/controller/CommentControllerTest.java
@@ -1,18 +1,30 @@
 package com.example.HomeService_MVC.controller;
 
+import com.example.HomeService_MVC.model.Customer;
+import com.example.HomeService_MVC.model.enumoration.Role;
+import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
@@ -23,6 +35,9 @@ class CommentControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    Customer customer = new Customer("farzadCustomer","afshar",
+            "fc@gmail.com","aA 1!aaa",300L, Role.ROLE_CUSTOMER);
+
     @BeforeAll
     static void setup(@Autowired DataSource dataSource) {
         try (Connection connection = dataSource.getConnection()) {
@@ -30,5 +45,23 @@ class CommentControllerTest {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    @Order(1)
+    public void placeAComment() throws Exception {
+        customer.setId(2);
+        JSONObject commentJson = new JSONObject();
+        commentJson.put("comment","best!");
+        commentJson.put("stars",5);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/comment/save/" + 1)
+                .with(SecurityMockMvcRequestPostProcessors.user(customer))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(commentJson.toJSONString());
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
     }
 }
