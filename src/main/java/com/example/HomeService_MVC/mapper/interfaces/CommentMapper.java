8288e01ef3107d36aee6f65272ae9a33b917308e
farzadafi <farzadafi40@gmail.com>
2022-07-23 16:06:35 +0430

    add @Mapper on the top of class

--- src/main/java/com/example/HomeService_MVC/mapper/interfaces/CommentMapper.java
+++ src/main/java/com/example/HomeService_MVC/mapper/interfaces/CommentMapper.java
@@ -1,9 +1,14 @@
 package com.example.HomeService_MVC.mapper.interfaces;
 
 import com.example.HomeService_MVC.dto.comment.CommentDto;
+import com.example.HomeService_MVC.mapper.impel.CommentMapperDecorator;
 import com.example.HomeService_MVC.model.Comment;
+import org.mapstruct.DecoratedWith;
+import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
+@Mapper
+@DecoratedWith(CommentMapperDecorator.class)
 public interface CommentMapper {
 
     CommentMapper INSTANCE = Mappers.getMapper(CommentMapper.class);
