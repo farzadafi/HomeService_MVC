44f2db6e4a0cca7f5eab1f049f1c2ed69abf9c29
farzadafi <farzadafi40@gmail.com>
2022-07-25 07:04:00 +0430

    add placeAnOffer method

--- src/test/java/com/example/HomeService_MVC/controller/OfferControllerTest.java
+++ src/test/java/com/example/HomeService_MVC/controller/OfferControllerTest.java
@@ -1,23 +1,42 @@
 package com.example.HomeService_MVC.controller;
 
+import com.example.HomeService_MVC.model.Expert;
+import com.example.HomeService_MVC.model.enumoration.Role;
+import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.*;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.jdbc.datasource.init.ScriptUtils;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 @SpringBootTest
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class OfferControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    Expert expert = new Expert("farzadExpert","afshar","fe@gmail.com","aA 1!aaa",0L,
+            Role.ROLE_EXPERT,"kerman",null,null);
 
     @BeforeAll
     static void setup(@Autowired DataSource dataSource) {
@@ -26,5 +45,24 @@ class OfferControllerTest {
         } catch (SQLException e) {
             e.printStackTrace();
         }
+    }
+
+    @Test
+    @Order(1)
+    public void placeAnOffer() throws Exception {
+        expert.setId(1);
+        JSONObject offerJson = new JSONObject();
+        offerJson.put("proposedPrice", 1000000);
+        offerJson.put("durationWork", 10);
+        offerJson.put("startTime", "10:10");
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/offer/placeAnOffer/" + 1 )
+                .with(SecurityMockMvcRequestPostProcessors.user(expert))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(offerJson.toJSONString());
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
     }
 }
