fbda6dc14cd8d8811f2b4ab955f92613e1a8ecff
farzadafi <farzadafi40@gmail.com>
2022-07-25 06:52:40 +0430

    add setup method for insert data to DB

--- src/test/java/com/example/HomeService_MVC/controller/OfferControllerTest.java
+++ src/test/java/com/example/HomeService_MVC/controller/OfferControllerTest.java
@@ -2,9 +2,16 @@ package com.example.HomeService_MVC.controller;
 
 import org.junit.jupiter.api.*;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
@@ -12,5 +19,12 @@ import org.springframework.test.context.junit4.SpringRunner;
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class OfferControllerTest {
 
-
+    @BeforeAll
+    static void setup(@Autowired DataSource dataSource) {
+        try (Connection connection = dataSource.getConnection()) {
+            ScriptUtils.executeSqlScript(connection, new ClassPathResource("OfferControllerData.sql"));
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
